7fca8c2749c049baae7666522919a663e468fe35
Seth Brummund <swbrummund@gmail.com>
2019-10-19 09:35:44 -0500

    complete ufo_level_1 ;

--- UFO_level_1/static/js/app.js
+++ UFO_level_1/static/js/app.js
@@ -4,34 +4,40 @@ var tableData = data;
 // YOUR CODE HERE!
 var tbody = d3.select("tbody");
   
-// tableData.forEach(function(ufoSighting) {
-//     console.log(ufoSighting);
-//     var row = tbody.append("tr");
-//     Object.entries(ufoSighting).forEach(function([key, value]) {
-//         console.log(key, value);
-//         var cell = row.append("td");
-//         cell.text(value);
-//     });
-// });
+tableData.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+    var row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
 
 
-var button = d3.select(".btn");
+var button = d3.select("#filter-btn");
 
-button.on("change",function() {
+
+button.on("click",function() {
+    // console.log("button change fired")
     var inputDate = d3.select("#datetime").property("value");
+    
     
     function filterDate(date) {
         return date.datetime === inputDate;
       };
 
+    console.log(inputDate);
     var filteredData = tableData.filter(filterDate);
-    console.log(filteredData);
+    // console.log(filteredData);
+
+    tbody.html("")
 
     filteredData.forEach(function(ufoSighting) {
-        console.log(ufoSighting);
+        // console.log(ufoSighting);
         var row = tbody.append("tr");
         Object.entries(ufoSighting).forEach(function([key, value]) {
-            console.log(key, value);
+            // console.log(key, value);
             var cell = row.append("td");
             cell.text(value);
         });
