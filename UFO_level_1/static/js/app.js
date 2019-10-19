20c89f90ce9f9eec9191012c3cc48541dfd9d0a2
Seth Brummund <swbrummund@gmail.com>
2019-10-19 08:22:18 -0500

    sat morn

--- UFO_level_1/static/js/app.js
+++ UFO_level_1/static/js/app.js
@@ -2,3 +2,39 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+  
+// tableData.forEach(function(ufoSighting) {
+//     console.log(ufoSighting);
+//     var row = tbody.append("tr");
+//     Object.entries(ufoSighting).forEach(function([key, value]) {
+//         console.log(key, value);
+//         var cell = row.append("td");
+//         cell.text(value);
+//     });
+// });
+
+
+var button = d3.select(".btn");
+
+button.on("change",function() {
+    var inputDate = d3.select("#datetime").property("value");
+    
+    function filterDate(date) {
+        return date.datetime === inputDate;
+      };
+
+    var filteredData = tableData.filter(filterDate);
+    console.log(filteredData);
+
+    filteredData.forEach(function(ufoSighting) {
+        console.log(ufoSighting);
+        var row = tbody.append("tr");
+        Object.entries(ufoSighting).forEach(function([key, value]) {
+            console.log(key, value);
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+
+});
